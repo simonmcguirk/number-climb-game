3a7684a0ca7a74c16a8f72f2baa707794fa648ef
Simon McGuirk <5030092+simonmcguirk@users.noreply.github.com>
2026-08-15 11:18:14 +0100

    Create iOS project

---
+++ NumberClimbGame/NumberClimbGame/NumberClimbGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberClimbGameApp.swift
+//  NumberClimbGame
+//
+//  Created by Simon McGuirk on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct NumberClimbGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
